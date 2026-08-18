3f6f5242be8d568f3dcf6cb23ed0c63332cd09ab
rustinpeace91 <isaaclubanko@comcast.net>
2026-08-18 08:17:45 -0400

    fix old indent issues

--- core/src/main/java/dungeon/crawler/Player/PlayerInputHandler.java
+++ core/src/main/java/dungeon/crawler/Player/PlayerInputHandler.java
@@ -4,32 +4,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 
 public class PlayerInputHandler {
-public PlayerDirection direction;
-public boolean movementInputPressed = false;
-public PlayerInputHandler(
-PlayerDirection direction
+    public PlayerDirection direction;
+    public boolean movementInputPressed = false;
+    public PlayerInputHandler(
+    PlayerDirection direction
 ) {
-this.direction = direction;
+    this.direction = direction;
 }
-// handle eventual key config here
-public void updateInput() {
-if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-this.direction = PlayerDirection.LEFT;
-this.movementInputPressed = true;
-}
-else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-this.direction = PlayerDirection.RIGHT;
-this.movementInputPressed = true;
-}
-else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-this.direction = PlayerDirection.UP;
-this.movementInputPressed = true;
-}
-else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-this.direction = PlayerDirection.DOWN;
-this.movementInputPressed = true;
-} else {
-this.movementInputPressed = false;
-}
-}
+    // handle eventual key config here
+    public void updateInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            this.direction = PlayerDirection.LEFT;
+            this.movementInputPressed = true;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            this.direction = PlayerDirection.RIGHT;
+            this.movementInputPressed = true;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            this.direction = PlayerDirection.UP;
+            this.movementInputPressed = true;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            this.direction = PlayerDirection.DOWN;
+            this.movementInputPressed = true;
+        } else {
+            this.movementInputPressed = false;
+        }
+    }
 }
